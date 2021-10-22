2cd606714d428722d8afbc374121cde87fc65c99
Shai Nagar <sha1n@users.noreply.github.com>
2021-10-22 23:37:32 +0300

    added title util func for setup/teardown (#6)

--- test_util.sh
+++ test_util.sh
@@ -8,6 +8,14 @@ function test_script_separator() {
   printf "%-80s\n" | tr ' ' '-'
 }
 
+function test_setup_title() {
+  printf " $fg_bold[cyan]> SETUP:$reset_color %s...\n" "$funcstack[-1]"
+}
+
+function test_teardown_title() {
+  printf " $fg_bold[cyan]> TEARDOWN:$reset_color %s...\n" "$funcstack[-1]"
+}
+
 function test_case_title() {
   printf " $fg_bold[cyan]> CASE:$reset_color %s...\n" "$funcstack[-1]"
 }
